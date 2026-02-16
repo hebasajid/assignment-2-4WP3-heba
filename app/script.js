1caabce25a99b2133eb98be023e4b4441580001a
Heba Sajid <hsajid35467@gmail.com>
2026-02-15 22:10:19 -0700

    implementing post route to get weather params

---
+++ app/script.js
@@ -0,0 +1,9 @@
+// by following the coinlore pattern provided in class:
+app.post("/get-weather", async function(req, res) { 
+    // getting the 3 params
+    const city = req.body.city;
+    const unitType = req.body.units;
+    const showWind = req.body.wind;
+
+    
+});
